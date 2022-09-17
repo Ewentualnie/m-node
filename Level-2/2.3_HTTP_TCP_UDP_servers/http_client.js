cf931f289ae27d10105e8d32837bc6230240076f
Serhii <ewentualnie.sz@gmail.com>
2022-09-17 19:01:37 +0300

    complete http client and server

---
+++ Level-2/2.3_HTTP_TCP_UDP_servers/http_client.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const port = 8001;
+const host = '127.0.0.1';
+const message = 'hello HTTP server';
+
+const post = {
+    hostname: host,
+    port: port,
+    method: 'POST'
+};
+
+let date = new Date();
+
+let request = http.request(post, res => {
+
+    res.on('data', data => {
+        console.log(`Send to server:  "${message}"`);
+        console.log(`Get from server: "${data}"`)
+        console.log(`comparison:      "${message == data}"`)
+    });
+
+    res.on('end', () => {
+        console.log(`Answer get after ${(new Date() - date) / 1000} sec.`);
+    });
+});
+
+request.write(message);
+request.end();
---
+++ Level-2/2.3_HTTP_TCP_UDP_servers/http_server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const port = 8001;
+const host = '127.0.0.1';
+
+const server = http.createServer().listen(port, host);
+
+server.on('connection', socket => {
+    console.log(`Client connected from: ${socket.localAddress}:${socket.localPort}`);
+});
+server.on('request', (req, res) => {
+    req.on('data', data => {
+        console.log(`Client send: "${data}"`)
+        this.data = data;
+    });
+
+    req.on('end', () => {
+        res.end(this.data);
+    });
+});
+
+console.log(`Server started on ${host}:${port}`);
