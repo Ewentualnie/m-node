8af0dfda90e564bf767fd7ebba7ce48ccd53221f
Serhii <ewentualnie.sz@gmail.com>
2022-09-17 20:33:16 +0300

    complete tcp client and server

--- Level-2/2.3_HTTP_TCP_UDP_servers/tcp_client.js
+++ Level-2/2.3_HTTP_TCP_UDP_servers/tcp_client.js
@@ -1,18 +1,23 @@
 const net = require('net');
 const port = 8000;
 const host = '127.0.0.1';
+const message = "hello TCP server"
+
+let date = new Date();
 
 const client = new net.Socket();
-client.connect(port, host, () => {
-    console.log('Connected with port ' + port + ", host is: " + host);
-    client.write('Hello, server!');
-});
+
+client.connect(port, host);
+
+client.write(message);
 
 client.on('data', data => {
-    console.log('Received from server: ' + data);
-    client.destroy(); // kill client after server's response
+    console.log(`Send to server:  "${message}"`);
+    console.log(`Get from server: "${data}"`)
+    console.log(`comparison:      "${message == data}"`)
 });
 
-client.on('close', () => {
-    console.log('Connection closed');
-});
+client.on('close', () =>
+    console.log(`Answer get after ${(new Date() - date) / 1000} sec.`));
+
+client.end();
--- Level-2/2.3_HTTP_TCP_UDP_servers/tcp_server.js
+++ Level-2/2.3_HTTP_TCP_UDP_servers/tcp_server.js
@@ -3,12 +3,16 @@ const port = 8000;
 const host = '127.0.0.1';
 
 const server = net.createServer(socket => {
-    socket.write('Hello client!\n');
-    socket.on("data", data => console.log("Server get: \"" + data + "\""))
+    socket.on("data", data => {
+        console.log(`Client send: "${data}"`);
+        this.data = data;
+    })
+    socket.on('end', () =>
+        socket.write(this.data));
 });
 
-function launchInfo() {
-    console.log("Server started on port: " + port + ", host name is: " + host)
-}
+server.on('connection', socket =>
+    console.log(`Client connected from: ${socket.localAddress}:${socket.localPort}`));
 
-server.listen(port, host, () => launchInfo())
+server.listen(port, host, () =>
+    console.log(`Server started on ${host}:${port}`));
