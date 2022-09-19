0c93196a6e982594204b1e5543ca9a18dadc652d
Serhii <ewentualnie.sz@gmail.com>
2022-09-19 14:05:03 +0300

    little fix

--- Level-2/2.3_HTTP_TCP_UDP_servers/tcp_client.js
+++ Level-2/2.3_HTTP_TCP_UDP_servers/tcp_client.js
@@ -3,18 +3,18 @@ const port = 8000;
 const host = '127.0.0.1';
 const message = "hello TCP server"
 
-let date = new Date();
+let date;
 
 const client = new net.Socket();
 
 client.connect(port, host);
 
-client.write(message);
+client.write(message, () => date = new Date());
 
 client.on('data', data => {
     console.log(`Send to server:  "${message}"`);
     console.log(`Get from server: "${data}"`)
-    console.log(`comparison:      "${message == data}"`)
+    console.log(`comparison:      "${message === data.toString()}"`)
 });
 
 client.on('close', () =>
