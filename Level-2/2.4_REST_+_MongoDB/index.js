312bacbb37458af67eb1ef3f41e1475acb40ff3a
Serhii Zaika <96259500+Ewentualnie@users.noreply.github.com>
2022-11-06 12:33:24 +0200

    Delete index.js

--- Level-2/2.4_REST_+_MongoDB/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import path from 'path'
-
-const __dirname = path.resolve()
-const port = process.env.PORT ?? 8000;
-const app = express()
-
-app.use(express.static('static'))
-app.listen(port, () => console.log("server started on port: " + port))
