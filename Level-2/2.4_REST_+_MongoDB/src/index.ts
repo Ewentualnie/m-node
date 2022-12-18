cca06be0bf366f0a24a471036ff206df2ac58ddf
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-18 17:13:23 +0200

    :sparkles: move some functional to separated routes, and import it

--- Level-2/2.4_REST_+_MongoDB/src/index.ts
+++ Level-2/2.4_REST_+_MongoDB/src/index.ts
@@ -1,7 +1,8 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
-let items = require('../routes/items.js');
 
+const apiV1 = require('../routes/items.js');
+const apiV2 = require('../routes/items2.js')
 const port = process.env.PORT ?? 3005;
 const app: Express = express();
 const session = require('express-session');
@@ -19,11 +20,11 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
 }));
-app.use('/api/v1/items', items);
+app.use('/api/v1', apiV1);
+app.use('/api/v2', apiV2)
 
 app.route('/api/v1/login')
     .post((req: Request, res: Response) => {
-        console.log(req.params)
         res.send(JSON.stringify(ok));
     });
 app.route('/api/v1/logout')
