be7b2284137fce85a9ba1ed125c651b6a5b45ec1
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-18 17:20:17 +0200

    :adhesive_bandage: clean up code

--- Level-2/2.4_REST_+_MongoDB/src/index.ts
+++ Level-2/2.4_REST_+_MongoDB/src/index.ts
@@ -1,14 +1,12 @@
-import express, {Express, Request, Response} from 'express'
+import express, {Express} from 'express'
 import cors from 'cors'
 
 const apiV1 = require('../routes/items.js');
-const apiV2 = require('../routes/items2.js')
+const apiV2 = require('../routes/items2.js');
 const port = process.env.PORT ?? 3005;
 const app: Express = express();
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-const ok: { ok: boolean } = {ok: true}
-
 
 app.use(express.static('static'));
 app.use(express.json());
@@ -23,16 +21,4 @@ app.use(session({
 app.use('/api/v1', apiV1);
 app.use('/api/v2', apiV2)
 
-app.route('/api/v1/login')
-    .post((req: Request, res: Response) => {
-        res.send(JSON.stringify(ok));
-    });
-app.route('/api/v1/logout')
-    .post((req: Request, res: Response) => {
-        res.send(JSON.stringify(ok));
-    });
-app.route('/api/v1/register')
-    .post((req: Request, res: Response) => {
-        res.send(JSON.stringify(ok));
-    });
 app.listen(port, () => console.log("server started on port: " + port))
