9f493878882c55ffb1065f5ec7c506e32b31f848
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-18 17:10:38 +0200

    :rocket: api v1, import functions from mongo.ts

--- Level-2/2.4_REST_+_MongoDB/routes/items.ts
+++ Level-2/2.4_REST_+_MongoDB/routes/items.ts
@@ -1,54 +1,14 @@
 import express, {Request, Response} from "express";
-import {MongoClient, UpdateResult, WithId} from "mongodb";
+import {load, add, edit, del} from './mongo'
 
 const router = express.Router();
-const path: string = '/';
-
-const ObjectId = require("mongodb").ObjectId;
-const client = new MongoClient("mongodb://root:root@localhost:27017");
-const collection = client.db("mongoDb").collection("todolist");
-
-type Element = { id: string, text: string, checked: boolean };
-
-router.get(path, async (req: Request, res: Response) => {
-    res.end(JSON.stringify({items: await loadDb()}));
-    await client.close();
-}).post(path, async (req: Request, res: Response)=> {
-    res.end(JSON.stringify({id: await addTask(req.body)}));
-    await client.close();
-}).put(path, async (req: Request, res: Response)=> {
-    res.end(JSON.stringify(await editTask(req.body)));
-    await client.close();
-}).delete(path, async (req: Request, res: Response)=> {
-    res.send(JSON.stringify({ok: await deleteTask(req.body)}));
-    await client.close();
-});
-
-async function loadDb(): Promise<WithId<Document>[]> {
-    await client.connect()
-    return collection.find({}).toArray().then()
-}
-
-async function addTask(element: { text: string }): Promise<typeof ObjectId> {
-    await client.connect()
-    return collection.insertOne({"text": element.text, "checked": false})
-        .then(result => result.insertedId)
-}
-
-async function editTask(task: Element): Promise<UpdateResult> {
-    await client.connect()
-    return collection.updateOne({_id: ObjectId(task.id)}, {$set: {text: task.text, checked: task.checked}});
-}
-
-async function deleteTask(task: Element): Promise<boolean> {
-    await client.connect()
-    return collection.deleteOne({_id: ObjectId(task.id)})
-        .then(result => result.acknowledged)
-}
-
-client.connect().then(() => {
-    console.log("connect to mongoDb is success")
-    client.close().then()
-});
-
+const path: string = '/items';
+const ok: { ok: boolean } = {ok: true}
+router.get(path, async (req: Request, res: Response) => res.end(JSON.stringify({items: await load()})))
+    .post(path, async (req: Request, res: Response) => res.end(JSON.stringify({id: await add(req.body)})))
+    .put(path, async (req: Request, res: Response) => res.end(JSON.stringify(await edit(req.body))))
+    .delete(path, async (req: Request, res: Response) => res.send(JSON.stringify({ok: await del(req.body)})))
+    .post('/login', (req: Request, res: Response) => res.send(JSON.stringify(ok)))
+    .post('/logout', (req: Request, res: Response) => res.send(JSON.stringify(ok)))
+    .post('/register', (req: Request, res: Response) => res.send(JSON.stringify(ok)));
 module.exports = router;
