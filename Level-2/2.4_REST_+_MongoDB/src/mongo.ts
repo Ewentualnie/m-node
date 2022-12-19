6e9f25527afaeb785959a7813d0735931c394a0b
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-20 01:02:23 +0200

    :hammer: replace arrow function expression to a traditional function expression

--- Level-2/2.4_REST_+_MongoDB/src/mongo.ts
+++ Level-2/2.4_REST_+_MongoDB/src/mongo.ts
@@ -6,20 +6,23 @@ const collection = client.db("mongoDb").collection("todolist");
 
 type Element = { id: string, text: string, checked: boolean };
 
-export const load = async (): Promise<WithId<Document>[]> => {
+export async function load(): Promise<WithId<Document>[]> {
     await client.connect()
     return collection.find({}).toArray().then()
 }
-export const add = async (element: { text: string }): Promise<typeof ObjectId> => {
+
+export async function add(element: { text: string }): Promise<typeof ObjectId> {
     await client.connect()
     return collection.insertOne({"text": element.text, "checked": false})
         .then(result => result.insertedId)
 }
-export const edit = async (task: Element): Promise<UpdateResult> => {
+
+export async function edit(task: Element): Promise<UpdateResult> {
     await client.connect()
     return collection.updateOne({_id: ObjectId(task.id)}, {$set: {text: task.text, checked: task.checked}});
 }
-export const del = async (task: Element): Promise<boolean> => {
+
+export async function del(task: Element): Promise<boolean> {
     await client.connect()
     return collection.deleteOne({_id: ObjectId(task.id)})
         .then(result => result.acknowledged)
