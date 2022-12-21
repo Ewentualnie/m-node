fe01501b26c1f9bf9b93cf669d7f132d759fdbca
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-21 15:45:40 +0200

    :seedling: add save users in Database, add work with many users

--- Level-2/2.4_REST_+_MongoDB/src/mongo.ts
+++ Level-2/2.4_REST_+_MongoDB/src/mongo.ts
@@ -1,34 +1,51 @@
-import {MongoClient, UpdateResult, WithId} from "mongodb";
+import {MongoClient, ObjectId, UpdateResult, WithId} from "mongodb";
 
-const ObjectId = require("mongodb").ObjectId;
 const client = new MongoClient("mongodb://root:root@localhost:27017");
-const collection = client.db("mongoDb").collection("todolist");
+const tasks = client.db("mongoDb").collection("todolist");
+const users = client.db("mongoDb").collection("users");
 
-type Element = { id: string, text: string, checked: boolean };
+type Element = { id: string, text: string, checked: boolean, userId: string };
+type User = { login: string, pass: string };
 
-export async function load(): Promise<WithId<Document>[]> {
+export async function load(userId: string): Promise<WithId<Document>[]> {
     await client.connect()
-    return collection.find({}).toArray().then()
+    return tasks.find({userId: new ObjectId(userId)}).toArray().then()
 }
 
-export async function add(element: { text: string }): Promise<typeof ObjectId> {
+export async function add(element: { text: string }, userId: string): Promise<ObjectId> {
     await client.connect()
-    return collection.insertOne({"text": element.text, "checked": false})
+    return tasks.insertOne({"text": element.text, "checked": false, userId: new ObjectId(userId)})
         .then(result => result.insertedId)
 }
 
-export async function edit(task: Element): Promise<UpdateResult> {
+export async function edit(task: Element, userId: string): Promise<UpdateResult> {
     await client.connect()
-    return collection.updateOne({_id: ObjectId(task.id)}, {$set: {text: task.text, checked: task.checked}});
+    return tasks.updateOne({_id: new ObjectId(task.id), userId: new ObjectId(userId)}, {
+        $set: {
+            text: task.text,
+            checked: task.checked
+        }
+    });
 }
 
-export async function del(task: Element): Promise<boolean> {
+export async function del(task: Element, userId: string): Promise<boolean> {
     await client.connect()
-    return collection.deleteOne({_id: ObjectId(task.id)})
+    return tasks.deleteOne({_id: new ObjectId(task.id), userId: new ObjectId(userId)})
         .then(result => result.acknowledged)
 }
 
+export async function addUser(user: User): Promise<ObjectId> {
+    await client.connect()
+    return users.insertOne({login: user.login, password: user.pass})
+        .then(result => result.insertedId)
+}
+
+export async function getUser(user: User) {
+    await client.connect()
+    return users.findOne({login: user.login, password: user.pass})
+}
+
 client.connect().then(() => {
-    console.log("Connect to mongoDb is success")
+    console.log("Connection to mongoDb is successful")
     client.close().then()
 });
