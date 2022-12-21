9eb3e685a4e23c4a7a44b61ac93b2e0f7f4ae64a
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-21 15:46:53 +0200

    :dizzy: replace functions with import this functions

--- Level-2/2.4_REST_+_MongoDB/src/routers/router_v1.ts
+++ Level-2/2.4_REST_+_MongoDB/src/routers/router_v1.ts
@@ -1,14 +1,14 @@
-import express, {Request, Response} from "express";
-import {getTasks, addTask, editTask, deleteTask} from "../controllers/dbcontroller";
+import express from "express";
+import {addTask, deleteTask, editTask, getTasks} from "../controllers/dbcontroller";
+import {login, logout, registration} from "../controllers/authcontroller";
 
 const router = express.Router();
 const path: string = '/items';
-const ok: { ok: boolean } = {ok: true}
 router.get(path, getTasks)
     .post(path, addTask)
     .put(path, editTask)
     .delete(path, deleteTask)
-    .post('/login', (req: Request, res: Response) => res.send(JSON.stringify(ok)))
-    .post('/logout', (req: Request, res: Response) => res.send(JSON.stringify(ok)))
-    .post('/register', (req: Request, res: Response) => res.send(JSON.stringify(ok)));
+    .post('/login', login)
+    .post('/logout', logout)
+    .post('/register', registration);
 export default router;
