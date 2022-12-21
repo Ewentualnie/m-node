49196871a0756dd2800be66ae50a1d9690aaea35
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-21 15:44:30 +0200

    :passport_control: replace session use

--- Level-2/2.4_REST_+_MongoDB/src/index.ts
+++ Level-2/2.4_REST_+_MongoDB/src/index.ts
@@ -6,19 +6,16 @@ import api_v2 from './routers/router_v2'
 
 const port = process.env.PORT ?? 3005;
 const app = express();
-const FileStore = require('session-file-store')(session);
-
-app.use(express.static('static'));
-app.use(express.json());
-app.use(cors());
-
+export const FileStore = require('session-file-store')(session);
 app.use(session({
     store: new FileStore({}),
     secret: 'keyboard cat',
     resave: true,
     saveUninitialized: true,
 }));
-
+app.use(express.static('static'));
+app.use(express.json());
+app.use(cors());
 app.use('/api/v1', api_v1);
 app.use('/api/v2', api_v2);
 
