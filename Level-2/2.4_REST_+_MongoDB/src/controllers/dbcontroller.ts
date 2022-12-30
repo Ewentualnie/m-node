ab68acb509f050d188a920105b08f6d13096bf8d
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-30 22:47:39 +0200

    :zap: add errors handlers

--- Level-2/2.4_REST_+_MongoDB/src/controllers/dbcontroller.ts
+++ Level-2/2.4_REST_+_MongoDB/src/controllers/dbcontroller.ts
@@ -3,22 +3,30 @@ import {Request, Response} from "express";
 import {FileStore} from './sessioncontroller'
 
 
-export async function getTasks(req: Request, res: Response) {
+export function getTasks(req: Request, res: Response) {
     if (FileStore[req.sessionID] != undefined && FileStore[req.sessionID].userId != undefined) {
-        res.end(JSON.stringify({items: await load(FileStore[req.sessionID].userId)}))
+        load(FileStore[req.sessionID].userId).then((value) => value ?
+            res.status(200).send(JSON.stringify({items: value})) :
+            res.status(404).send(JSON.stringify({error: "can't load tasks"})))
     } else {
-        res.end(JSON.stringify({error: "forbidden"}))
+        res.status(404).send(JSON.stringify({error: 'forbidden'}))
     }
 }
 
-export async function addTask(req: Request, res: Response) {
-    res.end(JSON.stringify({id: await add(req.body, FileStore[req.sessionID].userId)}))
+export function addTask(req: Request, res: Response) {
+    add(req.body, FileStore[req.sessionID].userId).then(value => value ?
+        res.status(200).send(JSON.stringify({id: value})) :
+        res.status(404).send(JSON.stringify({error: "can't add task"})))
 }
 
-export async function editTask(req: Request, res: Response) {
-    res.end(JSON.stringify(await edit(req.body, FileStore[req.sessionID].userId)))
+export function editTask(req: Request, res: Response) {
+    edit(req.body, FileStore[req.sessionID].userId).then(value => value ?
+        res.status(200).send(JSON.stringify(value)) :
+        res.status(404).send(JSON.stringify({error: "can't edit task"})))
 }
 
-export async function deleteTask(req: Request, res: Response) {
-    res.send(JSON.stringify({ok: await del(req.body, FileStore[req.sessionID].userId)}))
+export function deleteTask(req: Request, res: Response) {
+    del(req.body, FileStore[req.sessionID].userId).then(value => value ?
+        res.status(200).send(JSON.stringify({ok: value})) :
+        res.status(404).send(JSON.stringify({error: "can't delete task"})))
 }
