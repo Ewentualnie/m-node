45a3d440b1be073ed1a8b3a2d5a5146f71a3cac4
Serhii Zaika <ewentualnie.sz@gmail.com>
2022-12-30 22:47:57 +0200

    :zap: add errors handlers

--- Level-2/2.4_REST_+_MongoDB/src/controllers/authcontroller.ts
+++ Level-2/2.4_REST_+_MongoDB/src/controllers/authcontroller.ts
@@ -1,24 +1,30 @@
 import {Request, Response} from "express";
 import {addUser, getUser} from "../mongo";
-import {decrypt, encrypt} from "./pidcryptcontroller";
 import {FileStore} from './sessioncontroller'
+import generateHash from "./hashcontroller";
 
 const ok: { ok: boolean } = {ok: true}
 
-export async function login(req: Request, res: Response) {
-    const user = await getUser(req.body)
-    if (user && decrypt(user) == req.body.pass) {
-        FileStore[req.sessionID] = {userId: user._id}
-        res.send(JSON.stringify(ok))
-    } else {
-        res.send(JSON.stringify({error: 'not found'}))
-    }
+export function login(req: Request, res: Response): void {
+    getUser(req.body)
+        .then((user) => {
+            if (user && user.pass == generateHash(req.body)) {
+                FileStore[req.sessionID] = {userId: user._id}
+                res.status(200).send(JSON.stringify(ok))
+            } else {
+                res.status(404).send(JSON.stringify({error: 'not found'}))
+            }
+        })
 }
 
-export async function logout(req: Request, res: Response) {
-    res.send(JSON.stringify(ok))
+export function logout(req: Request, res: Response): void {
+    req.session.destroy(() =>
+        res.status(200).send(JSON.stringify(ok)));
 }
 
-export async function registration(req: Request, res: Response) {
-    res.send(JSON.stringify({ok: await addUser(req.body, encrypt(req.body))!= null}))
+export function registration(req: Request, res: Response) {
+    addUser(req.body, generateHash(req.body))
+        .then((exist) => exist ?
+            res.status(200).send(JSON.stringify(ok)) :
+            res.status(404).send(JSON.stringify({error: 'user already exists!'})))
 }
